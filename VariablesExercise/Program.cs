4210ba4aa66f7e33cdba3ffe1656124604c2a733
Carly Mead <meadcarly16@gmail.com>
2024-07-08 13:34:31 -0600

    Declared and initialized variables and interpolated them in the console

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myGoal = "to become proficient at CSharp";
+
+            int timeFrameWeeks = 9;
+
+            char grade = 'A';
+
+            bool amISucceeding = true;
+
+            double timeSpent = 30.5;
+
+            decimal totalCost = 7000.00m;
+            
+            Console.WriteLine($"My goal is {myGoal} in {timeFrameWeeks} weeks and to get an {grade}. If this comes {amISucceeding}, then the ${totalCost} will be worth it. So far, I have {timeSpent} hours in.");
+
         }
     }
 }
